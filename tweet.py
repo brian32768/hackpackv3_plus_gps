2d388419ca0b5127366ac4bf4727ad8ccf73d931
Brian H Wilson <brian@wildsong.biz>
2017-06-16 17:07:40 -0700

    added datestamp

--- tweet.py
+++ tweet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
-import os, sys
+import os
+from datetime import datetime
 import tweepy
 
 consumer_key    = os.environ["TWITTER_CONSUMER_KEY"]
@@ -8,11 +9,21 @@ consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
 access_key      = os.environ["TWITTER_ACCESS_KEY"]
 access_secret   = os.environ["TWITTER_ACCESS_SECRET"]
 
+def timestamp(fmt='%Y-%m-%d %H-%M-%S'):
+    return datetime.now().strftime(fmt)
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+api  = tweepy.API(auth)
 
-api.update_status('ANOTHER TEST from python script sent using #tweepy')
+# This tweets, so all your followers will see it.
+#api.update_status('ANOTHER TEST from python script sent using #tweepy')
 
+
+# This sends a direct message so only the user "@twiliotest" will see it.
+# If @twiliotest does not follow you the message won't go through.
+# So if you don't want to set up a separate twitter account then you should
+# follow @twiliotest and it will automatically follow you back.
+api.send_direct_message(user='@twiliotest', text='Hello from Python! ' + timestamp())
 exit(0)
 
